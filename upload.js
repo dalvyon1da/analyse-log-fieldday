77efc38db0d84de00193396fabc8f17b02172ebd
dalvyon1da <on1dal140271@gmail.com>
2026-06-12 14:21:58 +0200

    Add files via upload

--- upload.js
+++ upload.js
@@ -2,7 +2,7 @@ const uploadBtn = document.getElementById("uploadBtn");
 const fileInput = document.getElementById("fileInput");
 const uploadStatus = document.getElementById("uploadStatus");
 
-//  Mets TON TOKEN ici
+// 🔐 Mets TON TOKEN ici
 const GITHUB_TOKEN = "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";
 
 const REPO = "dalyon1da/analyse-log-fieldday";
@@ -19,6 +19,8 @@ uploadBtn.addEventListener("click", async () => {
     const content = await file.text();
     const path = `donnees/logs/${file.name}`;
 
+    const encoded = btoa(unescape(encodeURIComponent(content)));
+
     const response = await fetch(`https://api.github.com/repos/${REPO}/contents/${path}`, {
         method: "PUT",
         headers: {
@@ -27,7 +29,7 @@ uploadBtn.addEventListener("click", async () => {
         },
         body: JSON.stringify({
             message: `Upload log ${file.name}`,
-            content: btoa(content),
+            content: encoded,
             branch: BRANCH
         })
     });
